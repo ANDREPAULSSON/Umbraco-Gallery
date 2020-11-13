7cae9a6aa1619e48685fdc6b9c89c5c8e072ef6b
Andre Paulsson <paulsson_andre@hotmail.com>
2020-11-13 16:28:36 +0100

    Changed email value in googlemaps macro

--- Umbraco-Gallery/App_Data/Models/all.generated.cs
+++ Umbraco-Gallery/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "2dd77c3edbc1d4b1")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
